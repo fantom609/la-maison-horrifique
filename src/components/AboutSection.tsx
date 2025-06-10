d50ce835a2f483480e181b19e0a9872056ecdf8b
Fantom <arthur.morelon@gmail.com>
2025-06-10 10:27:05 +0200

    add AboutSection

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,11 @@
+export const AboutSection = () => {
+    return (
+        <section className="about">
+            <h2>À propos de nous</h2>
+            <p>
+                La maison horrifique est une société proposant des sessions d’escape game immersives sur le thème de l’horreur.
+                La société veut mettre en place un site web afin d’augmenter sa visibilité et gérer les réservations à ses différentes sessions de jeu.
+            </p>
+        </section>
+    );
+};
