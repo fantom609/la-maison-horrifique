6bc149e944e4fd41db4a5360d6095059818a9938
Baptiste LAURENT <bmg.laurent@live.fr>
2025-06-10 10:29:41 +0200

    add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import './Footer.css'
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-social">
+                <a href="#"><i className="fab fa-facebook"></i></a>
+                <a href="#"><i className="fab fa-instagram"></i></a>
+                <a href="#"><i className="fab fa-twitter"></i></a>
+            </div>
+            <div className="footer-links">
+                <Link to="/games">Sessions de jeu</Link>
+                <Link to="/legal">Mentions légales</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div className="footer-copyright">
+                © {new Date().getFullYear()} La Maison Horrifique
+            </div>
+        </footer>
+    );
+};
